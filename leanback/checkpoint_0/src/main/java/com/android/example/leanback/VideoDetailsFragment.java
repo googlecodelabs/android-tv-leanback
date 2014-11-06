7c113abd1ef1dcb9605442119c105981b1a0ee35
Anirudh Dewani <anirudhd@google.com>
2014-11-05 23:02:44 -0500

    details view for phone app

---
+++ leanback/checkpoint_0/src/main/java/com/android/example/leanback/VideoDetailsFragment.java
@@ -0,0 +1,65 @@
+package com.android.example.leanback;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.example.leanback.data.Video;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by anirudhd on 11/5/14.
+ */
+public class VideoDetailsFragment extends Fragment {
+    private View mView;
+
+    /**
+     * Create a new instance of MyDialogFragment, providing "num"
+     * as an argument.
+     */
+    static VideoDetailsFragment newInstance(String tag) {
+        VideoDetailsFragment f = new VideoDetailsFragment();
+
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putString("TAG", tag);
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_details, container, false);
+        return mView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Picasso.with(getActivity()).load(mVideo.getThumbUrl()).transform(BlurTransform.getInstance(this.getActivity())).fit().into((ImageView) mView.findViewById(R.id.image_view));
+        ((TextView)mView.findViewById(R.id.movie_info_title)).setText(mVideo.getTitle());
+        ((TextView)mView.findViewById(R.id.movie_info_text)).setText(mVideo.getDescription());
+    }
+
+    public Video getmVideo() {
+        return mVideo;
+    }
+
+    public void setmVideo(Video mVideo) {
+        this.mVideo = mVideo;
+    }
+
+    private Video mVideo;
+
+}
